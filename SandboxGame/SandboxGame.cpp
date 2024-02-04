c676108ad861a463f67bf4a8b4d68c4f656c69c2
Kulpot <kulpotwahu@gmail.com>
2024-02-04 17:14:39 +0800

    ...

--- SandboxGame/SandboxGame.cpp
+++ SandboxGame/SandboxGame.cpp
@@ -2,7 +2,7 @@
 //--------------- Making a Sandbox Game ---------------------
 //ref link:https://www.youtube.com/watch?v=_5JLA2-_0JM
 
-//Intermediate Directory  $(Configuration)\Temp\
+//SandboxGame->Intermediate Directory  $(Configuration)\Temp\
 //Engine1->ConfigProperties->GeneralProp->ConfigType->Dynamic Library(.dll)
 //Engine1->ConfigProp->AdvancesProp->TargetFileExtension->.dll
 
