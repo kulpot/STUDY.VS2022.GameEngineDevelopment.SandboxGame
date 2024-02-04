f65100ddcbfa9cf71c6373cf5fe551513130306d
Kulpot <kulpotwahu@gmail.com>
2024-02-04 18:32:37 +0800

    Adding Qt Middleware to SandboxGame

--- SandboxGame/SandboxGame.cpp
+++ SandboxGame/SandboxGame.cpp
@@ -6,6 +6,21 @@
 //Engine1->ConfigProperties->GeneralProp->ConfigType->Dynamic Library(.dll)
 //Engine1->ConfigProp->AdvancesProp->TargetFileExtension->.dll
 
+//------------------- Adding Qt Middleware to SandboxGame ------------------------------
+//ref link:https://www.youtube.com/watch?v=mkYMjk36QT8&t=105s
+
+/*
+* 1) Install VS Community Edition, which at the time of this posting was here:
+https://visualstudio.microsoft.com/downloads/
+2) Install Qt 4.8.4, which is what Jamie is using here. DO NOT get the MinGW version, as the static libs there will have the .a extension. My research showed that VS should be able to handle them now so they might work for you, but I could not make them work so I had to get the VS2010.exe download from here:
+https://download.qt.io/archive/qt/4.8/4.8.4/
+ I used the file "qt-win-opensource-4.8.4-vs2010.exe," and it installed just fine on Windows 10. 
+
+* 
+* 
+* 
+*/
+
 void main()
 {
 
